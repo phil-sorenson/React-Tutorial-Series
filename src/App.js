b8e524a894ceea5df5a7bb407e667673040c92ca
phil-sorenson <phillip.sorenson31@gmail.com>
2022-11-30 22:12:21 -0600

    Created DisplayEntries Component and Instatiated Component on App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
-
-
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+//⭐ When importing ⬆️ './' allows you to look at anything that is currently inside the same folder as the file youre importing in to (e.g Allowing us to see all thing inside of src folder)
+//⭐ DisplayEntries is a 'CHILD' of the APP component because it's being instantiated inside of the APP component
 function App() {
   
-  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}])
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Entry Number</th>
-            <th>Weight</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-      </table>
+      <DisplayEntries  parentEntries={entries}/>
+
     </div>
   );
 }
 
 export default App;
+
+//⭐ Passing data from one component down to another is done through PROPS (e.g. Passing 'entries' variable down to DisplayEntries from App.js)
+//❔Even when you're calling certain JS variables, a { } is needed? (E.g {entries})
---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,33 @@
+
+const DisplayEntries = (props) => {
+    return ( 
+        <table>
+            <thead>
+            <tr>
+                <th>Entry Number</th>
+                <th>Weight</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            {/* ( ) used because their will be multiple lines of code we want returned */}
+            {/* 'index' in our map func reresents the index number of the thing it's iterating/looping over (e.g [1]) */}
+            {/* Adding the {index + 1} allowed our <td> tags to be moved over 1 spot */}
+            {/* 'entries' gets changed to 'props.parentEntries.map' because we're taking the value from the state variable (entries), passing it down to DisplayEntries component, which has recieved a new key Value pair (parentEntries) */}
+                {/* parentEntries === entries */}
+            {props.parentEntries.map((entry, index)=> {
+                return (
+                <tr>
+                    <td>{index + 1}</td>
+                    <td>{entry.weight}</td>
+                    <td>{entry.date}</td>
+                </tr>
+
+                )
+            })}
+            </tbody>
+        </table>
+     );
+}
+ 
+export default DisplayEntries;
